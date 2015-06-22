a08b7e0d0c3c0af384bcb305910e3d4f67589c58
Will G <willg@1337hax.org>
2015-06-22 17:18:09 -0400

    Inital release

--- src/DCN/RBAC/Traits/HasRoleAndPermission.php
+++ src/DCN/RBAC/Traits/HasRoleAndPermission.php
@@ -167,7 +167,7 @@ trait HasRoleAndPermission
     protected function hasRole($role)
     {
         return $this->getRoles()->contains(function ($key, $value) use ($role) {
-            return $role == $value->id || str_is($role, $value->slug);
+            return $role == $value->id || Str::is($role, $value->slug);
         });
     }
     
@@ -230,7 +230,7 @@ trait HasRoleAndPermission
     protected function hasPermission($permission)
     {
         return $this->getPermissions()->contains(function ($key, $value) use ($permission) {
-            return $permission == $value->id || str_is($permission, $value->slug);
+            return $permission == $value->id || Str::is($permission, $value->slug);
         });
     }
 
